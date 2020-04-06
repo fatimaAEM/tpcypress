4c0f046d7beadaf04f10386a7395519bc00ca74a
AEM FatimaEzzahra <fatima.ezzahra096@gmail.com>
2020-04-06 22:33:08 +0100

    chore: refactoring code

--- cypress/integration/testJs.spec.js
+++ cypress/integration/testJs.spec.js
@@ -1,41 +1,65 @@
 describe('Test JS',function(){
 
+     // setup these constants to match what TodoMVC does
+    let TODO_ITEM_ONE = 'Javascript'
+    let TODO_ITEM_TWO = 'JEE'
+    let TODO_ITEM_THREE = 'PHP'
+
+    // -> Function for inser items
+    Cypress.Commands.add('constItems', (items) => {
+        for (var item = 0; item < items.length; item++) {
+            cy.get('.new-todo')
+            .type(items[item] + "{enter}")
+        }  
+    })
+
+    // -> Function for inser items with state
+    Cypress.Commands.add('inserItem', (list, etat) => {
+
+        for (var iter = 0; iter < list.length; iter++) {
+            cy.get('.new-todo')
+            .type(list[iter] + "{enter}")
+            if (list[iter][1] == "Active") {
+                cy.get('.todo-list li')
+                .eq(iter)
+                .find(".toggle")
+                .check()  
+            }
+
+            cy.get('.filters')
+            .contains(etat)
+            .click()
+        }
+        
+    })
+
     beforeEach(function () {
         cy.visit('http://todomvc.com/examples/jquery/')
     }) 
 	
-	// 1 - Ajouter un item:
     it('Should add item', function () {
         cy.get('.new-todo')
-        .type("Javascript{enter}")
+        .type(TODO_ITEM_ONE+"{enter}")
+
         cy.get('.todo-list li')
         .should("have.length", 1)
         .and("contain","Javascript")
-    }  
-    )
+    })
 	
-	// 2 - Supprimer un item
 	it('Should delete one item', function () {
-         cy.get('.new-todo')
-        .type("Javascript{enter}")
+        cy.get('.new-todo')
+        .type(TODO_ITEM_ONE+"{enter}")
+
         cy.get('.todo-list li')
         .should("contain","Javascript")
         .find(".destroy")
         .invoke("show")
         .click()
         .should('not.exist')
-
-    }
-    )   
+    })   
 	
-	// 3 - Supprimer tous les items
     it('Should delete all items', function () {
-        cy.get('.new-todo')
-        .type("Javascript{enter}")
-        cy.get('.new-todo')
-        .type("JEE{enter}")
-        cy.get('.new-todo')
-        .type("PHP{enter}")
+        cy.constItems([TODO_ITEM_ONE,TODO_ITEM_TWO,TODO_ITEM_THREE])
 
         cy.get('.todo-list li').each(function () {
             cy.get(".destroy").click({
@@ -45,81 +69,37 @@ describe('Test JS',function(){
         
         cy.get('.todo-list li')
         .should('have.length', 0)
-    }
-    )
-	
-	// 4 - Afficher les items par catégorie: 
-	// -> all
-	it('Should get all items', function () {
-        cy.get('.new-todo')
-        .type("Javascript{enter}")
-        cy.get('.new-todo')
-        .type("JEE{enter}")
-        cy.get('.todo-list li')
-        .eq(1)
-        .find(".toggle")
-        .check()
+    })
 
-        cy.get('.filters')
-        .contains("All")
-        .click()
+    // -> All
+    it('Should get all items', function () {
+        cy.inserItem([["Angular", "Active"], ["Laravel", "Completed"]], "All")
 
         cy.get('.todo-list li')
         .should('have.length', 2)
        
-    }
-    )
-	
-	// -> active
+    })
+    
+    // -> Active
     it('Should get active items', function () {
-        cy.get('.new-todo')
-        .type("Javascript{enter}")
-        cy.get('.new-todo')
-        .type("JEE{enter}")
-        cy.get('.todo-list li')
-        .eq(1)
-        .find(".toggle")
-        .check()
-
-        cy.get('.filters')
-        .contains("Active")
-        .click()
+        cy.inserItem([["Angular", "Active"], ["Laravel", "Completed"]], "Active")
 
         cy.get('.todo-list li')
         .should('have.length', 1)
        
-    }
-    )
+    })
 
-	// completed
+	// Completed
     it('Should get completed items', function () {
-        cy.get('.new-todo')
-        .type("Javascript{enter}")
-        cy.get('.new-todo')
-        .type("JEE{enter}")
-        cy.get('.todo-list li')
-        .eq(1)
-        .find(".toggle")
-        .check()
-
-        cy.get('.filters')
-        .contains("Completed")
-        .click()
+        cy.inserItem([["Angular", "Active"], ["Laravel", "Completed"]], "Completed")
 
         cy.get('.todo-list li')
         .should('have.length', 1)
        
-    }
-    )
+    })
 
-	// 5 - Supprimer les items complétés
     it('Should delete completed items', function () {
-        cy.get('.new-todo')
-        .type("Javascript{enter}")
-        cy.get('.new-todo')
-        .type("JEE{enter}")
-        cy.get('.new-todo')
-        .type("PHP{enter}")
+        cy.constItems([TODO_ITEM_ONE,TODO_ITEM_TWO,TODO_ITEM_THREE])
 
         cy.get('.todo-list li')
         .eq(1)
@@ -139,7 +119,5 @@ describe('Test JS',function(){
 
         cy.get('.todo-list li')
         .should('have.length', 0)       
-    }
-    )
-	
+    })
 })
